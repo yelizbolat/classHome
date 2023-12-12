cba82a61195a27c5389b1dfbb84b5dcb7238c415
yeliz601 <yeliz601@BPC11>
2023-12-12 11:28:06 +0300

    Proje dosyası ekle.

---
+++ classHome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classHome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ev ev = new Ev();
+            Console.WriteLine("oda sayisi : {0}", ev.odaSayisi);
+            Console.WriteLine("metre karesi {0}:", ev.metreKaresi);
+        }
+    }
+    class Ev 
+    {
+       public int odaSayisi {  get; set; }
+        public int metreKaresi { get; set; }
+        public Ev()
+        {
+            Console.Write("oda sayisini girin: ");
+            odaSayisi=Convert.ToInt32(Console.ReadLine());
+            Console.Write("metre karesini girin:");
+            metreKaresi=Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
